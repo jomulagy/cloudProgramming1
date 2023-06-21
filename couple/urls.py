fb144455128346023b4d2ffd0ec839ee371c97a8
김지훈 <87347699+jomulagy@users.noreply.github.com>
2023-06-21 17:25:47 +0900

    back3

--- couple/urls.py
+++ couple/urls.py
@@ -4,9 +4,12 @@ from .views import *
 app_name = "couple"
 
 urlpatterns = [
-    path("matching/",matching, name= "matching"),
+    path("matching1/",matching1, name= "matching1"),
+    path("matching2/<str:grade>/",matching2, name= "matching2"),
+    path("matching3/<int:grade>/<str:age>/",matching3, name= "matching3"),
+    path("matching4/<int:grade>/<str:age>/<str:major>/",matching4, name= "matching4"),
 
-    path("match/list/",matchList, name= "mathchList"),
+    path("match/list/<int:grade>/<str:age>/<str:major>/<str:address>/",matchList, name= "mathchList"),
     path("couple/create/",coupleCreate, name= "couplecreate"),
 
 ]
--- couple/views.py
+++ couple/views.py
@@ -2,21 +2,43 @@ from django.shortcuts import render, redirect
 from account.models import User
 from couple.models import Couple
 
-def matching(request):
+def matching1(request):
+
     return render(request,"matched2.html")
 
-def matchList(request):
+def matching2(request,grade):
+    context = {
+        "grade" : grade
+    }
+    return render(request,"matched3.html",context)
+
+def matching3(request,grade,age):
+    context = {
+        "grade": grade,
+        "age": age,
+    }
+    return render(request,"matched4.html",context)
+
+def matching4(request,grade,age,major):
+    context = {
+        "grade": grade,
+        "age": age,
+        "major": major,
+    }
+    return render(request,"matched5.html",context)
+
+def matchList(request,grade,age,major,address):
     if request.user.sex == "남":
         sex = "여"
     else:
         sex = "남"
 
     if request.GET.get("age") == "연하":
-        matched = User.objects.filter(sex = sex,grade = request.GET.get('grade'),age__gt = request.GET.get('age'),address = request.Get.get('address'),major = request.GET.get('major')).exclude(id = request.user.id)
+        matched = User.objects.filter(sex = sex,grade = grade,age__gt = age,address = address,major = major).exclude(id = request.user.id)
     elif request.GET.get("age") == "연상":
-        matched = User.objects.filter(sex = sex,grade = request.GET.get('grade'),age__lt = request.GET.get('age'),address = request.Get.get('address'),major = request.GET.get('major')).exclude(id = request.user.id)
+        matched = User.objects.filter(sex = sex,grade = grade,age__lt = age,address = address,major = major).exclude(id = request.user.id)
     else:
-        matched = User.objects.filter(sex = sex,grade = request.GET.get('grade'),age = request.GET.get('age'),address = request.Get.get('address'),major = request.GET.get('major')).exclude(id = request.user.id)
+        matched = User.objects.filter(sex = sex,grade = grade,age = age,address = address,major = major).exclude(id = request.user.id)
 
     context = {
         "matched":matched
